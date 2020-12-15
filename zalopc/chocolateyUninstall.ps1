4bacd219db1643c3870fb87d6643804d48b2c85b
Duc A. Hoang <anhduc.hoang1990@gmail.com>
2020-12-15 17:07:50 +0900

    add uninstall script for zalopc

---
+++ zalopc/chocolateyUninstall.ps1
@@ -0,0 +1,11 @@
+$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName   = 'zalopc'
+  softwareName  = 'Zalo PC'
+  fileType      = 'exe'
+  silentArgs    = "/S"
+  validExitCodes= @(0)
+}
+
+Uninstall-ChocolateyPackage @packageArgs
