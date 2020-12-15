d567b18ea09a5613111d4e82bf70695b700d7330
Duc A. Hoang <anhduc.hoang1990@gmail.com>
2020-12-15 14:58:38 +0900

    add 64bit support for zalopc

--- zalopc/README.md
+++
@@ -1,8 +0,0 @@
-# Create and push packages to Chocolatey
-
-Run in `cmd` as admin.
-
-```
-cpack
-cpush zalopc.<version>.nupkg --source https://push.chocolatey.org/
-```
--- zalopc/chocolateyInstall.ps1
+++ zalopc/chocolateyInstall.ps1
@@ -3,12 +3,13 @@ $ErrorActionPreference = 'Stop';
 $packageArgs = @{
   packageName   = 'zalopc'
   softwareName  = 'Zalo PC'
-  file          = $fileLocation
   fileType      = 'exe'
   silentArgs    = "/S"
   url           = 'https://res-zaloapp-aka.zdn.vn/hybrid/ZaloSetup-20.11.2.exe'
+  url64bit      = 'https://res-zaloapp-aka.zdn.vn/hybrid/ZaloSetup-20.11.2.exe'
   validExitCodes= @(0)
   checksum      = '9242478f8e56f9981f81573428d67f868cfb161f640ae2550a09a6e4069ebd4d'
+  checksum64    = '9242478f8e56f9981f81573428d67f868cfb161f640ae2550a09a6e4069ebd4d'
   checksumType  = 'sha256'
 }
 
