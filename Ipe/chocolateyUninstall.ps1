c0df2db56d7598329e49eddd8be16f960f75f329
Duc A. Hoang <anhduc.hoang1990@gmail.com>
2020-12-23 07:18:41 +0900

    repack ipe

--- Ipe/chocolateyUninstall.ps1
+++ Ipe/chocolateyUninstall.ps1
@@ -2,7 +2,7 @@ $ErrorActionPreference = 'Stop'; # stop on all errors
 
 $shell = New-Object -COM WScript.Shell
 $shortcutPath = "${env:SystemDrive}\ProgramData\Microsoft\Windows\Start Menu\Programs\Ipe.lnk"
-$installDir = $shell.CreateShortcut($shortcutPath).TargetPath | Split-Path -parent
+$installDir = $shell.CreateShortcut($shortcutPath).TargetPath | Split-Path -parent | Split-Path -parent
 
 Remove-Item $installDir -Recurse -Force -Confirm:$false
 Remove-Item $shortcutPath -Recurse -Force -Confirm:$false
