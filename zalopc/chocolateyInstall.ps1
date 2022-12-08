7e46888de7d46b2690bcdee8431452e22699b12e
Duc A. Hoang <anhduc.hoang1990@gmail.com>
2022-12-08 13:45:52 +0700

    fix sha256sum zalopc 22.11.1

--- zalopc/chocolateyInstall.ps1
+++ zalopc/chocolateyInstall.ps1
@@ -7,7 +7,7 @@ $packageArgs = @{
   silentArgs    = "/S"
   url           = 'https://res-download-pc-te-vnno-pt-1.zadn.vn/hybrid/ZaloSetup-22.11.1.exe'
   validExitCodes= @(0, 3010, 1641)
-  checksum      = '4c0d3ba21eded33c9b1a4ca0c5243a9d3495a301fcc1bd1c6a429481e7c0e0d9' # 22.11.1
+  checksum      = 'd8cbf928f5bc0bb5eda21b1998da26c89bfed840db529152a2d4f1453aa7c80a' # 22.11.1
   checksumType  = 'sha256'
 }
 
