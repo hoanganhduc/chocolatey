4fcdb659ad6aab5f199df98faa2cab3471864225
Duc A. Hoang <anhduc.hoang1990@gmail.com>
2023-02-25 07:39:23 +0700

    fix errors pdfarranger, softether-vpn-client caused by autohotkey.portable

--- zalopc/chocolateyInstall.ps1
+++ zalopc/chocolateyInstall.ps1
@@ -5,9 +5,9 @@ $packageArgs = @{
   softwareName  = 'Zalo PC'
   fileType      = 'exe'
   silentArgs    = "/S"
-  url           = 'https://res-download-pc-te-vnno-pt-4.zadn.vn/hybrid/ZaloSetup-23.1.1.exe'
+  url           = 'https://res-download-pc-te-vnno-pt-4.zadn.vn/hybrid/ZaloSetup-23.2.1.exe'
   validExitCodes= @(0, 3010, 1641)
-  checksum      = '1b474f30a83b9db9fb2bb282054c145355080350c8970496b094b61c0a31da5a' # 23.1.1
+  checksum      = '0e323efd6271704f5c40b547ff943dc0a212e6bc1a9bb1cb1e5765db3c0ce974' # 23.2.1
   checksumType  = 'sha256'
 }
 
