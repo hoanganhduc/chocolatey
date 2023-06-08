efbfb01cf0077c3dddded78d6e49544e30c8edb6
Duc A. Hoang <anhduc.hoang1990@gmail.com>
2023-06-08 07:02:56 +0700

    pack ipe 7.2.27

--- Ipe/chocolateyInstall.ps1
+++ Ipe/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 $ErrorActionPreference = 'Stop';
 
 $pp = Get-PackageParameters
-$pkgver = "7.2.26"
+$pkgver = "7.2.27"
 
 if ($pp['InstallDir']) {
 	$installDir = $pp['InstallDir']
@@ -14,11 +14,9 @@ $packageArgs = @{
   softwareName   = 'Ipe extensible drawing editor'
   fileType       = 'zip'
   unziplocation  = "$installDir"
-  url            = "https://github.com/otfried/ipe/releases/download/v$pkgver/ipe-$pkgver-win32.zip"
   url64bit       = "https://github.com/otfried/ipe/releases/download/v$pkgver/ipe-$pkgver-win64.zip"
   validExitCodes = @(0, 3010, 1641)
-  checksum       = '3e67e901587508b20e8daf1c311c90036acc6408cc6fdc5b75b4cf32d973e772' 
-  checksum64     = '8e175570d8e0a83dfc490bbb9115f07196b015d4d249af64417c2faeffff3834'
+  checksum64     = 'e3ef969f48eb000c8a6c4445cc4ebc2e582f50b601794c6dc1fb4ef459734db3'
   checksumType   = 'sha256'
 }
 
