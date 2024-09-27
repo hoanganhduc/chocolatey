ed0b9cb7eb9e221b8a920ac62f62e76b77f1e9c6
Duc A. Hoang <anhduc.hoang1990@gmail.com>
2024-09-27 07:37:08 +0700

    pack zalopc 24.9.3

--- zalopc/chocolateyInstall.ps1
+++ zalopc/chocolateyInstall.ps1
@@ -5,9 +5,9 @@ $packageArgs = @{
   softwareName  = 'Zalo PC'
   fileType      = 'exe'
   silentArgs    = "/S"
-  url           = 'https://res-download-pc-te-vnno-ne-5.zadn.vn/win/ZaloSetup-24.8.5.exe'
+  url           = 'https://res-download-pc-te-vnno-ne-5.zadn.vn/win/ZaloSetup-24.9.3.exe'
   validExitCodes= @(0, 3010, 1641)
-  checksum      = 'f3a931da67fe23ca2d7c07964cf9fe0c40905829c0b20e684b3570ad78ee941b' # 24.8.5
+  checksum      = '2a5fd2f3399c4d6ea7e19687199c8af665282ee5e66a478b03424462c3eee12b' # 24.9.3
   checksumType  = 'sha256'
 }
 
