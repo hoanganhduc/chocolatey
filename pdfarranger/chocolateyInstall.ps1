2e209ef937b77eb9cc4037b9ac06437fc65f61c1
Duc A. Hoang <anhduc.hoang1990@gmail.com>
2024-10-19 20:22:32 +0700

    pack pdfarranger 1.11.1

--- pdfarranger/chocolateyInstall.ps1
+++ pdfarranger/chocolateyInstall.ps1
@@ -7,8 +7,8 @@ $packageArgs = @{
   softwareName  = 'pdfarranger'
   fileType      = 'MSI'
   silentArgs    = ""
-  url           = 'https://github.com/pdfarranger/pdfarranger/releases/download/1.10.0/pdfarranger-1.10.0-windows-installer.msi'
-  checksum      = 'b77a5dc7c956f1e0986664b0587b9b7532ca304590f4f1c86b03960486dde4f6' # 1.10.0
+  url           = 'https://github.com/pdfarranger/pdfarranger/releases/download/1.11.1/pdfarranger-1.11.1-windows-installer.msi'
+  checksum      = '5c1470a03bcda346093bc4c55c016220ebd54ae3edc7d5be9e586b5254264b67' # 1.11.1
   checksumType  = 'sha256'
 }
 
